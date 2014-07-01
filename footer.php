c5fb6a2f5befc5c935ca5b3e876f7bde2b086f77
clear-y <shout@jakecleary.net>
2014-07-01 21:35:24 +0100

    Add wp_head/footer & utilise constants for filepaths

--- footer.php
+++ footer.php
@@ -3,5 +3,6 @@
 
         </footer>
     </div>
+    <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -7,17 +7,19 @@
 
     <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/public/styles/main.css">
+    <link rel="stylesheet" type="text/css" href="<?php print STYLES_DIR . '/main.css'; ?>">
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script language="javascript" type="text/javascript" src="<?php bloginfo('template_directory'); ?>/public/js/functions.min.js"></script>
+    <script language="javascript" type="text/javascript" src="<?php print SCRIPTS_DIR . '/functions.min.js'; ?>"></script>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <?php wp_head(); ?>
 </head>
 <body class="">
     <div class="page-wrapper">
