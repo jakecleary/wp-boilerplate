e447c2948cd227ccd0676665c6ab77971eab3741
clear-y <shout@jakecleary.net>
2014-08-07 23:45:04 +0100

    Add post type interface
    We're going to do this properly! We can create an interface to enforce a uniform structure for all post types. Woo!

--- lib/PostTypes/PostTypeInterface.php
+++ lib/PostTypes/PostTypeInterface.php
@@ -3,6 +3,12 @@
 interface PostTypeInterface {
 
     /**
+     * Create the new post type
+     * @param Array $args An array of arguments
+     */
+    public function __construct(Array $args);
+
+    /**
      * Paginate the items of a post type
      * @param Object $queryData The $wp_query object
      */
